01ac796423e5149c28f28b2d3e22584a8882c296
Demetre Arjania <demetrearjania@Demetres-MacBook-Air.local>
2025-07-19 15:53:10 +0400

    🛡️ Add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password123'),
+                'uid' => Str::uuid(),
+            ]
+        );
+    }
+}
